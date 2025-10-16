130fa83a7fd58a3e285493f53bdca51b11d3df6d
Luiz Fernando de Lima <luizfernandofpl@gmail.com>
2025-10-16 18:53:35 -0300

    Update load_data to new processed data

--- falsb4mpa/dataset/load_data.py
+++ falsb4mpa/dataset/load_data.py
@@ -7,47 +7,26 @@ import pandas as pd
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-VALID_DATA_NAMES = ['adult', 'adult-race', 'german', 'titanic', 'heritage-health', 'adult-mpa', 'adult-mpa-balanced']
+VALID_DATA_NAMES = ["adult-mpa-bin-agg", "adult-mpa-bin-wout-agg"]
 VALID_FILE_NAMES = {
-    'adult':'adult', 
-    'adult-race':'adult', 
-    'german':'german', 
-    'titanic':'titanic', 
-    'heritage-health':'heritage-health',
-    'adult-mpa': 'adult_mpa',
-    'adult-mpa-balanced': 'adult_mpa_balanced'
+    "adult-mpa-bin-agg": "adult_mpa_bin_with_agg",
+    "adult-mpa-bin-wout-agg": "adult_mpa_bin_wout_agg",
 }
-VALID_FOLDER_NAMES = {
-    'adult':'adult', 
-    'adult-race':'adult', 
-    'german':'german', 
-    'titanic':'titanic', 
-    'heritage-health':'heritage-health',
-    'adult-mpa': 'adult',
-    'adult-mpa-balanced': 'adult'
-}
-VALID_LEARNING_STEPS = ['train', 'valid', 'test']
+VALID_FOLDER_NAMES = {"adult-mpa-bin-agg": "adult", "adult-mpa-bin-wout-agg": "adult"}
+VALID_LEARNING_STEPS = ["train", "valid", "test"]
 ACCESS_INDEXES = {
-    'adult': [slice(-1), -1, -2],
-    'adult-race':[slice(-1), -1, slice(63, 68)],
-    'german':[slice(-1), -1, -2],
-    'titanic':[slice(-1), -1, -2],
-    'heritage-health':[],
-    'adult-mpa':[slice(-1), -1, slice(106, 116)],
-    'adult-mpa-balanced':[slice(-1), -1, slice(106, 116)]
+    # dataset-name: [X, Y, A]
+    "adult-mpa-bin-agg": [slice(-1), -1, 1],  # A como subconjunto de X
+    "adult-mpa-bin-wout-agg": [slice(-1), -1, slice(1, 3)],
 }
 DIMENSIONS = {
-    'adult': [112, 1, 1], #[X, Y, A]
-    'adult-race': [112, 1, 5], 
-    'german': [31, 1, 1], 
-    'titanic': [19, 1, 1], 
-    'heritage-health':'heritage-health',
-    'adult-mpa': [116, 1, 10],
-    'adult-mpa-balanced': [116, 1, 10]
+    # dataset-name: [X, Y, A]
+    "adult-mpa-bin-agg": [116, 1, 1],
+    "adult-mpa-bin-wout-agg": [116, 1, 2],
 }
 
 
-def load_data(data_name, learning_step=None, kind='np'):
+def load_data(data_name, learning_step=None, kind="np"):
     """Function to load data.
 
     Args:
@@ -58,28 +37,31 @@ def load_data(data_name, learning_step=None, kind='np'):
         [type]: [description]
     """
     if not data_name in VALID_DATA_NAMES:
-        print('Invalid data name! Input: {} | Valid data names: [{}]',format(VALID_DATA_NAMES))
+        print("Invalid data name! Input: {} | Valid data names: [{}]", format(VALID_DATA_NAMES))
         return None
 
     if learning_step is None:
         learning_step = VALID_FILE_NAMES[data_name]
 
     elif not learning_step in VALID_LEARNING_STEPS:
-        print('Invalid data name! Input: {} | Valid steps: [{}]',format(VALID_LEARNING_STEPS))
+        print("Invalid data name! Input: {} | Valid steps: [{}]", format(VALID_LEARNING_STEPS))
         return None
 
     data_folder = select_data_folder(data_name)
     access_indexes = get_access_indexes(data_name)
-    
-    if kind == 'np':
-        #x, y, a = select_data_step_np(learning_step, access_indexes, data_folder, data_name)
+
+    if kind == "np":
+        # x, y, a = select_data_step_np(learning_step, access_indexes, data_folder, data_name)
         return select_data_step_np(learning_step, access_indexes, data_folder, data_name)
-    elif kind == 'pd':
-        #x, y, a = select_data_step_pd(learning_step, access_indexes, data_folder, data_name)
+    elif kind == "pd":
+        # x, y, a = select_data_step_pd(learning_step, access_indexes, data_folder, data_name)
         return select_data_step_pd(learning_step, access_indexes, data_folder, data_name)
 
+
 def select_data_folder(data_name):
-    return os.path.join(ROOT_DIR, Path(f'../../data/processed/{VALID_FOLDER_NAMES[data_name]}'.format()))
+    return os.path.join(
+        ROOT_DIR, Path(f"../../data/processed/{VALID_FOLDER_NAMES[data_name]}".format())
+    )
 
 
 def get_access_indexes(data_name):
@@ -87,19 +69,20 @@ def get_access_indexes(data_name):
 
 
 def select_data_step_np(learning_step, access_indexes, data_folder, data_name):
-    file = os.path.join(data_folder, Path(f'{learning_step}.csv'))
-    data = np.genfromtxt(file, delimiter=',', skip_header=True)[:, 1:]
+    file = os.path.join(data_folder, Path(f"{learning_step}.csv"))
+    data = np.genfromtxt(file, delimiter=",", skip_header=True)[:, 1:]
 
     num_examples = data.shape[0]
     x = data[:, access_indexes[0]]
     y = data[:, access_indexes[1]].reshape(num_examples, DIMENSIONS[data_name][1])
     a = data[:, access_indexes[2]].reshape(num_examples, DIMENSIONS[data_name][2])
-    
+
     return x, y, a
 
+
 def select_data_step_pd(learning_step, access_indexes, data_folder, data_name):
-    file = os.path.join(data_folder, Path(f'{learning_step}.csv'))
-    
+    file = os.path.join(data_folder, Path(f"{learning_step}.csv"))
+
     data = pd.read_csv(file)
-    
+
     return data
